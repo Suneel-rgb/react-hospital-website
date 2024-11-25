fac798a8d96b0153484ba8ccddf449f855eed488
Suneel Garnepudi <61911808+Suneel-rgb@users.noreply.github.com>
2024-11-25 19:03:50 +0530

    time Slots Completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import SpecialityMemu from "../components/SpecialityMemu";
+import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMemu />
+      <TopDoctors />
+      <Banner />
     </div>
   );
 };
